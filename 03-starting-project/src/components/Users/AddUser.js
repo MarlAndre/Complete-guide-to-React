d50491cb84e9102a54c14c0d836a9028c036988c
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-16 13:00:45 -0400

    adding managing error state and fixing small bugs

--- 03-starting-project/src/components/Users/AddUser.js
+++ 03-starting-project/src/components/Users/AddUser.js
@@ -8,15 +8,24 @@ function AddUser(props) {
   // array destructuring
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     // Validation logic. Check if a username && age are empty
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age. Fields can't be empty.",
+      });
       return;
     }
     // Validation logic. Check if entered age is < 1. + sign assure it is a number we are checking
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -32,9 +41,19 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -51,7 +70,7 @@ function AddUser(props) {
             value={enteredAge}
             onChange={ageChangeHandler}
           />
-          <Button type="submit">Add a new user </Button>
+          <Button type="submit">Add User</Button>
         </form>
       </Card>
     </div>
