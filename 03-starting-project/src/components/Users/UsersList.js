5fc3b2740455915355afc49e03082296e95e18a8
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-16 11:57:59 -0400

    adding UsersList component

---
+++ 03-starting-project/src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/Card";
+
+import classes from "./UsersList.module.css";
+
+function UsersList(props) {
+  return (
+    // Users is an array of user object
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li> // each objects has a name and age property
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default UsersList;
