f29220f95d691ea906845834f9181534f2d73700
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-16 11:44:11 -0400

    adding validation logic

--- 03-starting-project/src/components/Users/AddUser.js
+++ 03-starting-project/src/components/Users/AddUser.js
@@ -10,7 +10,18 @@ function AddUser(props) {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    // Validation logic. Check if a username && age are empty
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    // Validation logic. Check if entered age is < 1. + sign assure it is a number we are checking
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    // Resetting logic. Add value to input field
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -24,9 +35,19 @@ function AddUser(props) {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add a new user </Button>
       </form>
     </Card>
